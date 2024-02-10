3062942436fae094c3297bace8789641387f4cf0
Endalkachew-Tedla <EndalkachewTedla@outlook.com>
2024-02-10 09:36:19 +0500

    Initial commit

---
+++ js/autoTypingAnimation.js
@@ -0,0 +1,10 @@
+setTimeout(function() {
+    $('.about-info-inner').typed({
+      strings: [
+        `<span>Hello, I'm Endalkachew Tedla</span> a market research and product management analyst with 2+ years of experience, transitioning to the financial services industry. Completed CareerFoundry Data Analytics bootcamp (2024) and proficient in Excel, SQL, Python, Tableau, GitHub, and Data Ethics. Expert in insights and strategy.
+      `
+      ],
+      typeSpeed: -20,
+      contentType: 'html'
+    });
+  }, 0);
